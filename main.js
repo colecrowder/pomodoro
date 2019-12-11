6a0ab44d53d71ca48c49b323f2dac2da98960579
Jaycro <colecrowder@gmail.com>
2019-12-10 18:47:49 -0800

    Finished clock functions

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
-/*
-let minutes = 25;
-let seconds = 00;
-let currentTime = minutes.toFixed(2).toString + ":" + seconds.toFixed(2).toString;
-
-document.querySelector('#timer').innerHTML = currentTime; 
-
-function countdown() {
-    let timer = minutes*60+seconds
-    let start = Date.now();
-    while (timer > 0) {
-        if (start - Date.now() <= -1000000) {
-            start = Date.now();
-            console.log(timer)
-            timer--;
-        }
-    }
-}
-
-*/
 
 let paused = false;
 let running = false;
 let reset2 = false;
+let canBreak = true;
+
+let initialSeconds = 60 * 25;
+const display = document.querySelector('#timer');
 
 const stop = document.querySelector("#stop");
 stop.addEventListener("click", function() {
     paused = true;
+    running = false;
+    clearInterval(ourInterval);
+    arr = display.innerHTML.split(":");
+    initialSeconds = parseInt(arr[0], 10) * 60 + parseInt(arr[1], 10);
 });
 
 const start = document.querySelector("#start");
@@ -41,12 +29,16 @@ const reset = document.querySelector("#reset");
 reset.addEventListener("click", function() {
     reset2 = true;
     running = false;
-})
+    display.innerHTML = stime.innerHTML + ":00";
+    initialSeconds = parseInt(stime.innerHTML, 10)*60;
+});
+
+let ourInterval;
 
 function startTimer(duration, display) {
     running = true;
     var timer = duration, minutes, seconds;
-    let ourInterval = setInterval(function () {
+    ourInterval = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -55,19 +47,51 @@ function startTimer(duration, display) {
         display.textContent = minutes + ":" + seconds;
         if (reset2 === true) {
             clearInterval(ourInterval);
-            document.querySelector('#timer').innerHTML = "25:00"
+            display.innerHTML = initialSeconds / 60 + ":00";
         }
         if (paused === false) {
            if (--timer < 0) {
-                timer = duration;
                 running = false;
+                if (canBreak === true) {
+                    timer = parseInt(btime.innerHTML, 10)*60;
+                    canBreak = false;
+                } else {
+                    display.innerHTML = stime.innerHTML + ":00";
+                }
             }
         }
     }, 1000);
-}
+};
+
 
 let startCountdown = function () {
-    var initialMinutes = 60 * 25,
-        display = document.querySelector('#timer');
-    startTimer(initialMinutes, display);
+    startTimer(initialSeconds, display);
 };
+
+const stime = document.querySelector('#stime');
+
+const sarrup = document.querySelector('#sarrup');
+sarrup.addEventListener("click", function() {
+    initialSeconds = parseInt(stime.innerHTML, 10)*60 + 60;
+    stime.textContent = initialSeconds / 60;
+    if(running === false) { display.innerHTML = initialSeconds / 60 + ":00" };
+});
+
+const sarrdo = document.querySelector('#sarrdo');
+sarrdo.addEventListener("click", function() {
+    initialSeconds = parseInt(stime.innerHTML, 10)*60 - 60;
+    stime.textContent = initialSeconds / 60;
+    if(running === false) { display.innerHTML = initialSeconds / 60 + ":00" };
+});
+
+const btime = document.querySelector('#btime');
+
+const barrup = document.querySelector('#barrup');
+barrup.addEventListener("click", function () {
+    btime.innerHTML = parseInt(btime.innerHTML, 10) + 1;
+});
+
+const barrdo = document.querySelector('#barrdo');
+barrdo. addEventListener("click", function() {
+    btime.innerHTML = parseInt(btime.innerHTML, 10) - 1;
+});
