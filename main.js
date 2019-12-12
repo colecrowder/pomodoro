fb8a01c02cd8222b4c08e6f41a630323fad95280
Moritz Hofmann <mowmoh@gmail.com>
2019-12-12 02:34:30 +0100

    finish functionality and some design

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
 let paused = false;
 let running = false;
-let reset2 = false;
 let canBreak = true;
 
 let initialSeconds = 60 * 25;
@@ -19,7 +17,6 @@ stop.addEventListener("click", function() {
 const start = document.querySelector("#start");
 start.addEventListener("click", function() {
     paused = false;
-    reset2 = false;
     if (running === false) {
         startCountdown();
     }
@@ -27,44 +24,45 @@ start.addEventListener("click", function() {
 
 const reset = document.querySelector("#reset");
 reset.addEventListener("click", function() {
-    reset2 = true;
     running = false;
-    display.innerHTML = stime.innerHTML + ":00";
+    clearInterval(ourInterval);
+    display.innerHTML = (stime.innerHTML + ":00").padStart(5, '0');
     initialSeconds = parseInt(stime.innerHTML, 10)*60;
 });
 
 let ourInterval;
 
-function startTimer(duration, display) {
+function startTimer(initialSeconds, display) {
     running = true;
-    var timer = duration, minutes, seconds;
+
     ourInterval = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+        minutes = parseInt(initialSeconds / 60, 10);
+        seconds = parseInt(initialSeconds % 60, 10);
 
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
         display.textContent = minutes + ":" + seconds;
-        if (reset2 === true) {
-            clearInterval(ourInterval);
-            display.innerHTML = initialSeconds / 60 + ":00";
-        }
+
         if (paused === false) {
-           if (--timer < 0) {
+            if (--initialSeconds < 0) {
+                clearInterval(ourInterval);
+                audio.play();
                 running = false;
                 if (canBreak === true) {
-                    timer = parseInt(btime.innerHTML, 10)*60;
+                    initialSeconds = parseInt(btime.innerHTML, 10)*60;
+                    startTimer(initialSeconds, display);
                     canBreak = false;
                 } else {
-                    display.innerHTML = stime.innerHTML + ":00";
+                    display.innerHTML = (stime.innerHTML + ":00").padStart(5, '0');
+                    canBreak = true;
                 }
             }
         }
     }, 1000);
 };
 
-
 let startCountdown = function () {
+    clearInterval(ourInterval);
     startTimer(initialSeconds, display);
 };
 
@@ -74,14 +72,20 @@ const sarrup = document.querySelector('#sarrup');
 sarrup.addEventListener("click", function() {
     initialSeconds = parseInt(stime.innerHTML, 10)*60 + 60;
     stime.textContent = initialSeconds / 60;
-    if(running === false) { display.innerHTML = initialSeconds / 60 + ":00" };
+    if(running === false && paused === false) { 
+        display.innerHTML = (initialSeconds / 60 + ":00").padStart(5,'0');
+    };
 });
 
 const sarrdo = document.querySelector('#sarrdo');
 sarrdo.addEventListener("click", function() {
-    initialSeconds = parseInt(stime.innerHTML, 10)*60 - 60;
-    stime.textContent = initialSeconds / 60;
-    if(running === false) { display.innerHTML = initialSeconds / 60 + ":00" };
+    if(parseInt(stime.innerHTML, 10) >= 2 ) {
+        initialSeconds = parseInt(stime.innerHTML, 10)*60 - 60;
+        stime.textContent = initialSeconds / 60;
+        if(running === false && paused === false) { 
+                display.innerHTML = (initialSeconds / 60 + ":00").padStart(5, '0');
+        };
+    };
 });
 
 const btime = document.querySelector('#btime');
@@ -93,5 +97,9 @@ barrup.addEventListener("click", function () {
 
 const barrdo = document.querySelector('#barrdo');
 barrdo. addEventListener("click", function() {
-    btime.innerHTML = parseInt(btime.innerHTML, 10) - 1;
+    if(btime.innerHTML !== "1") {
+        btime.innerHTML = parseInt(btime.innerHTML, 10) - 1;
+    }
 });
+
+const audio = new Audio('analog-watch-alarm_daniel-simion.mp3');
